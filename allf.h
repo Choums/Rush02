a478e2ca8903b0f2c88601343ab794fe9982e152
Chams <chams.aidel@outlook.fr>
2020-07-18 17:40:26 +0200

    prot

---
+++ allf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   allf.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caidel <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/18 16:35:54 by caidel            #+#    #+#             */
+/*   Updated: 2020/07/18 17:19:40 by caidel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEF_ALLF
+#define DEF_ALLF
+
+#include <unistd.h>
+#include <stdio.h> //Pour les tests NE PAS OUBIER DE LE RETIRER
+#include <stdlib.h>
+#include <fcntl.h> //Pour open
+
+void	ft_print_err();
+int		ft_nice_dic();
+
+#endif
